aa1b315c8651ba2c90f27dd3ac3b5d725556e90b
ValeZumZum-Laptop <valeriaccolombo@gmail.com>
2017-01-21 09:16:11 -0300

    ya no me acuerdo que estoy commiteando porque lo hice anoche :D

---
+++ unity/Assets/Scrips/CowSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowSpawner : MonoBehaviour 
+{
+	public GameObject cowPrefab;
+
+	public float closeFromX;
+	public float closeToX;
+	public float closeFromZ;
+	public float closeToZ;
+	public float farFromX;
+	public float farToX;
+	public float farFromZ;
+	public float farToZ;
+
+	private float timer;
+
+	private void Start()
+	{
+		timer = Random.Range (0f, 2f);
+	}
+
+	private void Update()
+	{
+		timer -= Time.fixedDeltaTime;
+
+		if (timer <= 0)
+		{
+			SpawnCow ();
+		}
+	}
+
+	private void SpawnCow()
+	{
+		timer = Random.Range (8f, 10f);
+
+		GameObject cow = Instantiate (cowPrefab);
+		cow.transform.SetParent (transform.parent);
+		cow.transform.localPosition = transform.localPosition;
+
+		Vector3 force;
+		if (Random.Range (0, 10) < 5)
+		{
+			force = new Vector3 (Random.Range(farFromX, farToX), 0, Random.Range(farFromZ, farToZ));
+		}
+		else
+		{
+			force = new Vector3 (Random.Range(closeFromX, closeToX), 0, Random.Range(closeFromZ, closeToZ));
+		}
+
+		cow.GetComponent<Rigidbody> ().AddForce (force, ForceMode.Impulse);
+	}
+}
---
+++ unity/Assets/Scrips/Lechegedon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lechegedon : MonoBehaviour 
+{
+	public GameObject meteoritePrefab;
+
+	private bool letItBeRain = false;
+
+	public void StartLechegedon()
+	{
+		letItBeRain = true;
+	}
+
+	private void Update()
+	{
+		if (letItBeRain)
+		{
+			if (Random.Range (0, 100) < 1)
+			{
+				GameObject lechazo = Instantiate (meteoritePrefab);
+				meteoritePrefab.transform.SetParent (transform.parent);
+				meteoritePrefab.transform.localPosition = new Vector3 (Random.Range (-8f, 8f), 10, Random.Range (-14f, 14f));
+			}
+		}
+	}
+}

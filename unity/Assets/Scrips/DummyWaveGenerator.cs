b8efabb122feb580509f760b8802e01d49902e7b
ValeZumZum-Laptop <valeriaccolombo@gmail.com>
2017-01-20 23:08:11 -0300

    comportamiento de onda empujando vaca (con click)

---
+++ unity/Assets/Scrips/DummyWaveGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyWaveGenerator : MonoBehaviour 
+{
+	public GameObject wavePrefab;
+
+	private void GenerateWave(Vector3 wavePos)
+	{
+		GameObject wave = Instantiate (wavePrefab);
+		wave.transform.SetParent (transform.parent);
+		wave.transform.position = wavePos;
+	}
+
+	private float cooldown = 0;
+
+	void Update()
+	{
+		cooldown -= Time.fixedDeltaTime;
+
+		if(Input.GetMouseButtonDown(0) && (cooldown < 0))
+		{
+			cooldown = 2;
+
+			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+			RaycastHit hit;
+			bool collided = Physics.Raycast (ray, out hit);
+			if (collided) 
+			{
+				GenerateWave(new Vector3(hit.point.x, 0, hit.point.z));
+			}
+		}
+	}
+}
---
+++ unity/Assets/Scrips/Wave.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour 
+{
+	private float timeToLive;
+
+	void Start()
+	{
+		timeToLive = 0;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name.IndexOf ("vaca") > -1)
+		{
+			GameObject bomb = col.gameObject;
+
+			Vector3 waveCenter = transform.localPosition;
+			Vector3 bombCenter = bomb.transform.localPosition;
+
+			waveCenter.y = 0;
+			bombCenter.y = 0;
+
+			Vector3 direction = new Vector3(bombCenter.x - waveCenter.x, 0, bombCenter.z - waveCenter.z);
+			float distance = Vector3.Distance(bombCenter, waveCenter);
+			float extraImpulse = Mathf.Max (0.1f, (4 - distance));
+			Vector3 force = new Vector3(direction.x * extraImpulse, extraImpulse, direction.z * extraImpulse);
+
+			Debug.Log ("waveCenter: "+ waveCenter + " / bombCenter: "+bombCenter + " / distance: " +direction +" / magnitude: " + distance + " / force: " + force);
+
+			Rigidbody rb = bomb.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.zero;
+			rb.AddForce (force, ForceMode.Impulse);
+		}
+	}
+
+	void Update()
+	{
+		timeToLive += Time.fixedDeltaTime;
+		if (timeToLive > 2)
+		{
+			Destroy (gameObject);
+		}
+	}
+}

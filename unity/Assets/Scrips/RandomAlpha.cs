8ee007adf6dcfc3859241a64a04f6177b0f056b8
Lorena Weder <latamakuchi@gmail.com>
2017-01-21 18:43:15 -0300

    leche colorizada random + update del prefab del player que faltaba

---
+++ unity/Assets/Scrips/RandomAlpha.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAlpha : MonoBehaviour
+{
+    public float minAlpha = 0.3f;
+
+	void OnEnable()
+    {
+        var renderer = GetComponent<SpriteRenderer>();
+        var color = renderer.color;
+        color.a = Random.Range(minAlpha, 1f);
+        renderer.color = color;
+	}
+}

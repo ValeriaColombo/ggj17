79ca9076900fec4431aa4fcb7ce4942fad99242e
Lorena Weder <latamakuchi@gmail.com>
2017-01-21 23:21:27 -0300

    soundmanager

---
+++ unity/Assets/Scrips/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+    private static SoundManager instance;
+
+    public AudioSource effectsSource;
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void PlayEffect(AudioClip clip)
+    {
+        effectsSource.PlayOneShot(clip);
+    }
+}

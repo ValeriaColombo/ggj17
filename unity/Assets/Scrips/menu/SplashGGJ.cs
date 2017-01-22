4601b368cea848aaa48172e3e828b0cf15e09d3e
ValeZumZum-Laptop <valeriaccolombo@gmail.com>
2017-01-21 21:23:31 -0300

    Agrego splash de #GGJ17 :D

---
+++ unity/Assets/Scrips/menu/SplashGGJ.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashGGJ : MonoBehaviour {
+
+	private float timer = 0;
+
+	void Update () 
+	{
+		timer += Time.fixedDeltaTime;
+		if (timer > 2) 
+		{
+			SceneManager.LoadScene ("menu");
+		}
+	}
+}

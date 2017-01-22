0fb96795946c47fb09d125d907f508c74714ed7a
Lorena Weder <latamakuchi@gmail.com>
2017-01-21 21:29:43 -0300

    update de numeros del countdown de la bomba

---
+++ unity/Assets/Scrips/UpdateCounterNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCounterNumber : MonoBehaviour
+{
+    public Image image;
+    public Sprite[] sprites;
+
+    public void UpdateNumbers(float value)
+    {
+        int index = Mathf.FloorToInt(value * sprites.Length);
+        image.sprite = sprites[index];
+    }
+}
